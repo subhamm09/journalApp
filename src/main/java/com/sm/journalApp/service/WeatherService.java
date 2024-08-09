384e41e729c6d371affa50d6141e5d501b89ad97
subhamm <Newspaper@1234>
2024-08-09 10:43:06 +0530

    application properties related changes

--- src/main/java/com/sm/journalApp/service/WeatherService.java
+++ src/main/java/com/sm/journalApp/service/WeatherService.java
@@ -2,6 +2,7 @@ package com.sm.journalApp.service;
 
 import com.sm.journalApp.model.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -10,14 +11,16 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class WeatherService {
 
-    private static final String apiKey = "b4e805d9fb9f0d7fb140ae4cccc2850";
+    @Value("${weather.api.key}")
+    private String apiKey;
+
     private static final String API = "http://api.weatherstack.com/current?accesskey=API_KEY&query=CITY";
 
     @Autowired
     private RestTemplate restTemplate;
 
     public WeatherResponse getWeather(String city) {
-        String finaApi = API.replace("CITY", city).replace("API_KEY", city);
+        String finaApi = API.replace("CITY", city).replace("API_KEY", apiKey);
 //        HttpHeaders headers = new HttpHeaders();
 //        headers.set("key", "value");
 //        String requestBody = "{\n" +
